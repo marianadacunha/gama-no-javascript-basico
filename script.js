6061fd9c502d7280ad7c66577ffe932812b7babc
marianadacunha <marianadacunhamarques@gmail.com>
2020-05-26 20:37:02 +0200

    separando numeros e digitos verificadores

--- script.js
+++ script.js
@@ -2,14 +2,24 @@ console.log("JavaScript carregado");
 
 function validaCPF(cpf) {
     console.log(cpf.length)
-    if(cpf.length != 11) { // se o número de caracteres for de diferente de 11, retorna falso
+    if(cpf.length != 11) { // se o número de caracteres for diferente de 11, retorna falso
         return false; 
-    }   
+    }
     else {
-        var numeros = substring(0 , 9) // 0 é o primeiro número digitado, 1 é o segundo, etc...
-        return true;
+        var numeros = cpf.substring(0 , 9) // puxa os números da posição 0-9; '0' é o 1º número digitado, '1' é o 2º, etc...
+        var digitoVerificador = cpf.substring(9) // puxa os números da posição 9 em diante
+        var soma = 0;
+
+        console.log('Número do CPF ' + numeros);
+        console.log('Dígito do CPF ' + digitoVerificador);  
+        for (var i = 10; i > 1; i--) {
+            soma += numeros.charAt(10 - i) * i;
+
+        }
+            return true;
     }
 }
+
 
 function validacao() {
     console.log("Iniciando validação de CPF");
