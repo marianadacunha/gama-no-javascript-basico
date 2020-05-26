40bfffa034f8e007e796da7ae77a6a00a8de8854
marianadacunha <marianadacunhamarques@gmail.com>
2020-05-26 11:13:14 +0200

    fazendo o botao funcionar

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 console.log("JavaScript carregado");
 
+function validaCPF(cpf) {
+    return true;
+}
+
 function validacao() {
     console.log("Iniciando validação de CPF");
+    
     var cpf = document.getElementById("cpf_digitado").value;
-    console.log(cpf);
+    
+    var resultadoValidacao = validaCPF(cpf);
+
+    if (resultadoValidacao) {
+        document.getElementById("sucess").style.display = 'block';
+    }
 }
